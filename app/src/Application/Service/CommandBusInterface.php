061adc0129dde13476593065d8b0e459ef9d1d3c
IgorOlikov <i.olikov@mail.ru>
2024-11-23 12:39:09 +0500

    updated application project structure/configs, installed main budles,

---
+++ app/src/Application/Service/CommandBusInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Application\Service;
+
+interface CommandBusInterface
+{
+
+}
---
+++ app/src/UI/Controller/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\UI\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccountController extends AbstractController
+{
+    public function __construct(
+
+    ){}
+
+    public function createAccount()
+    {
+
+    }
+
+}
